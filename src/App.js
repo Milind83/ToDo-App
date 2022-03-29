81ba47ec9703ee5c9b8d9fc48d457b54f7ce2347
Milind <milindkhariwale83@gamil.com>
2022-03-29 13:17:26 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -9,11 +9,59 @@ function App() {
   const [todolist, setTodoList] = useState([]);
 
   const addTodo = (todoitem, date) => {
-    setTodoList([...todolist, { id: uuidv4(), name: todoitem, date }]);
+    setTodoList([
+      ...todolist,
+      { id: uuidv4(), name: todoitem, date, isCompleted: false },
+    ]);
     localStorage.setItem(
       "Todos-list",
-      JSON.stringify([...todolist, { id: uuidv4(), name: todoitem, date }])
+      JSON.stringify([
+        ...todolist,
+        { id: uuidv4(), name: todoitem, date, isCompleted: false },
+      ])
     );
+  };
+  // setting task completed/not completed
+  const toggleComplete = (id, checked) => {
+    const newData = todolist.map((data) =>
+      data.id === id ? { ...data, isCompleted: checked } : data
+    );
+    console.log(newData);
+    setTodoList(newData);
+    localStorage.setItem("Todos-list", JSON.stringify(newData));
+  };
+
+  // edit task continue...
+  const editTodo = (id) => {
+    console.log(id);
+    Swal.fire({
+      title: "Edit ToDo",
+      html:
+        '<input id="swal-input1" class="swal2-input" placeholder="Enter new todo name">' +
+        '<input type="date" id="swal-input2" class="swal2-input placeholder="Enter new todo name"">',
+      focusConfirm: false,
+      preConfirm: () => {
+        return [
+          document.getElementById("swal-input1").value,
+          document.getElementById("swal-input2").value,
+        ];
+      },
+    }).then((res) => {
+      console.log(res.value);
+      const newData = todolist.map((data) =>
+        data.id === id
+          ? { ...data, name: res.value[0], date: res.value[1] }
+          : data
+      );
+      console.log(newData);
+      if (res.value[0] === "" || res.value[1] === "") {
+        Swal.fire("Error!", "All fields are required", "error");
+      } else {
+        setTodoList(newData);
+        localStorage.setItem("Todos-list", JSON.stringify(newData));
+        Swal.fire("Edited!", "Your task is edited.", "success");
+      }
+    });
   };
   const removeTodo = (id) => {
     Swal.fire({
@@ -31,7 +79,7 @@ function App() {
         );
         setTodoList(temptArray);
         localStorage.setItem("Todos-list", JSON.stringify(temptArray));
-        Swal.fire("Deleted!", "Your file has been deleted.", "success");
+        Swal.fire("Deleted!", "Your task has been deleted.", "success");
       }
     });
   };
@@ -52,7 +100,12 @@ function App() {
     <div className="wrapper">
       <header>Todo App</header>
       <Form addTodo={addTodo} />
-      <Todos todolist={todolist} removeTodo={removeTodo} />
+      <Todos
+        todolist={todolist}
+        removeTodo={removeTodo}
+        toggleComplete={toggleComplete}
+        editTodo={editTodo}
+      />
       <div className="footer">
         <span>
           You have <span className="pendingTasks">{todolist.length}</span>{" "}
--- src/Componts/Todos.jsx
+++ src/Componts/Todos.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Todos({ todolist, removeTodo }) {
+function Todos({ todolist, removeTodo, editTodo, toggleComplete }) {
   return (
     <ul className="todoList">
       {todolist.map((data) => {
@@ -16,10 +16,29 @@ function Todos({ todolist, removeTodo }) {
               <div className="date__year">{year}</div>
               <div className="date__day">{day}</div>
             </div>
-            <div className="todo-name">{data.name}</div>
-            <span className="icon" onClick={() => removeTodo(data.id)}>
-              <i className="fas fa-trash fa-lg"></i>
-            </span>
+            <div
+              className={`todo-name ${data.isCompleted ? "strike-text" : ""} `} // if  is completedtrue then striketesxt added
+            >
+              {data.name}
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                className="custom-checkbox"
+                checked={data.isCompleted}
+                onChange={() => {
+                  toggleComplete(data.id, !data.isCompleted); //  true value change in false and reverse
+                }}
+              />
+            </div>
+            <div className="icons">
+              <span className="icon-edit" onClick={() => editTodo(data.id)}>
+                <i class="fas fa-edit fa-lg"></i>
+              </span>
+              <span className="icon" onClick={() => removeTodo(data.id)}>
+                <i className="fas fa-trash fa-lg"></i>
+              </span>
+            </div>
           </li>
         );
       })}
